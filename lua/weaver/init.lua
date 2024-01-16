68253606ef83850eaebd07ae40273c5300a45632
Rishit <rishpandey8097@gmail.com>
2024-01-17 02:01:27 +0530

    fix: functions

--- lua/weaver/init.lua
+++ lua/weaver/init.lua
@@ -8,24 +8,24 @@ M._is_window_open = false
 --- @field width integer?
 --- @field use_filename_as_weaver_title boolean?
 local WinProperties = {
-    height = 30,
-    width = 90,
-    use_filename_as_weaver_title = false,
+  height = 30,
+  width = 90,
+  use_filename_as_weaver_title = false,
 }
 
 M.win_properties = WinProperties
 
 --- @param win_properties WinProperties?
-M.setup = function(win_properties)
-    if win_properties then
-        M.win_properties = {
-            height = win_properties.height or M.win_properties.height,
-            width = win_properties.width or M.win_properties.width,
-            use_filename_as_weaver_title = win_properties.use_filename_as_weaver_title
-                or M.win_properties.use_filename_as_weaver_title,
-        }
-    end
-    require("weaver.commands")
+function M.setup(win_properties)
+  if win_properties then
+    M.win_properties = {
+      height = win_properties.height or M.win_properties.height,
+      width = win_properties.width or M.win_properties.width,
+      use_filename_as_weaver_title = win_properties.use_filename_as_weaver_title
+          or M.win_properties.use_filename_as_weaver_title,
+    }
+  end
+  require("weaver.commands")
 end
 
 return M
--- lua/weaver/ui.lua
+++ lua/weaver/ui.lua
@@ -9,30 +9,30 @@ local win = nil
 --- @param width integer
 --- @param win_title string?
 --- @return integer
-M.show_window = function(opts, cb, height, width, win_title)
-    local borderchars =
-        { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+function M.show_window(opts, cb, height, width, win_title)
+  local borderchars =
+  { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
 
-    win = popup.create(opts, {
-        title = win_title ~= nil and win_title ~= "" and win_title or "Weaver",
-        highlight = "WeaverWindow",
-        line = math.floor(((vim.o.lines - height) / 2) - 1),
-        col = math.floor((vim.o.columns - width) / 2),
-        minheight = height,
-        minwidth = width,
-        borderchars = borderchars,
-        callback = cb,
-    })
+  win = popup.create(opts, {
+    title = win_title ~= nil and win_title ~= "" and win_title or "Weaver",
+    highlight = "WeaverWindow",
+    line = math.floor(((vim.o.lines - height) / 2) - 1),
+    col = math.floor((vim.o.columns - width) / 2),
+    minheight = height,
+    minwidth = width,
+    borderchars = borderchars,
+    callback = cb,
+  })
 
-    local bufnr = vim.api.nvim_win_get_buf(win)
-    return bufnr
+  local bufnr = vim.api.nvim_win_get_buf(win)
+  return bufnr
 end
 
-M.close_window = function()
-    if win == nil then
-        return
-    end
-    vim.api.nvim_win_close(win, true)
+function M.close_window()
+  if win == nil then
+    return
+  end
+  vim.api.nvim_win_close(win, true)
 end
 
 return M
