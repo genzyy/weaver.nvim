3cbb3c92f0bc910b42e6cccb614b912ee716f67a
Rishit <rishpandey8097@gmail.com>
2024-01-17 01:59:03 +0530

    fix: weaver titles

--- lua/weaver/ui.lua
+++ lua/weaver/ui.lua
@@ -10,29 +10,29 @@ local win = nil
 --- @param win_title string?
 --- @return integer
 M.show_window = function(opts, cb, height, width, win_title)
-  local borderchars =
-  { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+    local borderchars =
+        { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
 
-  win = popup.create(opts, {
-    title = win_title or "Weaver",
-    highlight = "WeaverWindow",
-    line = math.floor(((vim.o.lines - height) / 2) - 1),
-    col = math.floor((vim.o.columns - width) / 2),
-    minheight = height,
-    minwidth = width,
-    borderchars = borderchars,
-    callback = cb,
-  })
+    win = popup.create(opts, {
+        title = win_title ~= nil and win_title ~= "" and win_title or "Weaver",
+        highlight = "WeaverWindow",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minheight = height,
+        minwidth = width,
+        borderchars = borderchars,
+        callback = cb,
+    })
 
-  local bufnr = vim.api.nvim_win_get_buf(win)
-  return bufnr
+    local bufnr = vim.api.nvim_win_get_buf(win)
+    return bufnr
 end
 
 M.close_window = function()
-  if win == nil then
-    return
-  end
-  vim.api.nvim_win_close(win, true)
+    if win == nil then
+        return
+    end
+    vim.api.nvim_win_close(win, true)
 end
 
 return M
--- lua/weaver/utils.lua
+++ lua/weaver/utils.lua
@@ -9,94 +9,94 @@ local buf_map = weaver._stack
 --- @param buf number
 --- @return string
 M._format_index = function(buf)
-  return string.format("_%d", buf)
+    return string.format("_%d", buf)
 end
 
 --- @return number
 M.get_current_buffer = function()
-  return vim.api.nvim_get_current_buf()
+    return vim.api.nvim_get_current_buf()
 end
 
 --- @param is_global boolean | nil
 M.open_weaver = function(is_global)
-  local curr_buf
-  if is_global == true then
-    curr_buf = 0
-  else
-    curr_buf = M.get_current_buffer()
-  end
-  local contents = buf_map[M._format_index(curr_buf)]
+    local curr_buf
+    if is_global == true then
+        curr_buf = 0
+    else
+        curr_buf = M.get_current_buffer()
+    end
+    local contents = buf_map[M._format_index(curr_buf)]
 
-  if contents == nil then
-    contents = {}
-  end
+    if contents == nil then
+        contents = {}
+    end
 
-  weaver._is_window_open = true
-  local win_opts = {}
-  local cb = function(_, _sel) end
+    weaver._is_window_open = true
+    local win_opts = {}
+    local cb = function(_, _sel) end
 
-  local win_title
+    local win_title
 
-  if is_global then
-    win_title = "Global"
-  elseif weaver.win_properties.use_filename_as_weaver_title then
-    win_title = vim.fn.expand("%")
-  end
+    if is_global then
+        win_title = "Global"
+    elseif weaver.win_properties.use_filename_as_weaver_title then
+        win_title = vim.fn.expand("%")
+    end
 
-  bufnr = ui.show_window(
-    win_opts,
-    cb,
-    weaver.win_properties.height,
-    weaver.win_properties.width,
-    win_title
-  )
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
-  local quit_command = ":CloseWeaver"
-  if is_global == true then
-    quit_command = quit_command .. " g"
-  end
-  vim.api.nvim_buf_set_keymap(
-    bufnr,
-    "n",
-    "q",
-    quit_command .. "<CR>",
-    { silent = true }
-  )
+    bufnr = ui.show_window(
+        win_opts,
+        cb,
+        weaver.win_properties.height,
+        weaver.win_properties.width,
+        win_title
+    )
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
+    local quit_command = ":CloseWeaver"
+    if is_global == true then
+        quit_command = quit_command .. " g"
+    end
+    vim.api.nvim_buf_set_keymap(
+        bufnr,
+        "n",
+        "q",
+        quit_command .. "<CR>",
+        { silent = true }
+    )
 end
 
 --- @param is_global boolean | nil
 M.close_weaver = function(is_global)
-  weaver._is_window_open = false
-  local buf_content = M.get_buffer_lines()
-  ui.close_window()
-  local curr_buf
-  if is_global == true then
-    curr_buf = 0
-  else
-    curr_buf = M.get_current_buffer()
-  end
-  buf_map[M._format_index(curr_buf)] = buf_content
+    weaver._is_window_open = false
+    local buf_content = M.get_buffer_lines()
+    ui.close_window()
+    local curr_buf
+    if is_global == true then
+        curr_buf = 0
+    else
+        curr_buf = M.get_current_buffer()
+    end
+    buf_map[M._format_index(curr_buf)] = buf_content
 end
 
 --- @param is_global boolean
 M.destroy_weaver = function(is_global)
-  local curr_buf
-  if is_global == true then
-    curr_buf = 0
-  else
-    curr_buf = M.get_current_buffer()
-  end
+    local curr_buf
+    if is_global == true then
+        curr_buf = 0
+    else
+        curr_buf = M.get_current_buffer()
+    end
 
-  buf_map[M._format_index(curr_buf)] = nil
+    buf_map[M._format_index(curr_buf)] = nil
 end
 
 --- @return table
 M.get_buffer_lines = function()
-  if bufnr == nil then
-    return {}
-  end
+    if bufnr == nil then
+        return {}
+    end
 
-  return vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    return vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 end
 
 return M
